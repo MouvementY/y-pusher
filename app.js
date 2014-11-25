362d225cd722bb5b4b18a2cda809e3cc038468bf
Pierre Dulac <dulacpier@gmail.com>
2014-11-25 18:13:05 +0100

    Convert indentation to spaces and add some comments

--- app.js
+++ app.js
@@ -8,59 +8,65 @@ var env = process.env.NODE_ENV || "development";
 var url = require("url");
 
 var redisClient = null;
+
+// Configuration
 if (env === "production") {
 
-	// Sentry
-	app.use(raven.middleware.express(process.env.RAVEN_DNS));
+    // Sentry
+    app.use(raven.middleware.express(process.env.RAVEN_DNS));
 
-	// Redis
-	var redisConf = url.parse(process.env.REDISCLOUD_URL),
-		redisAuth = (redisConf.auth || '').split(':'),
-		redisPass = redisAuth[1];
+    // Redis
+    var redisConf = url.parse(process.env.REDISCLOUD_URL),
+        redisAuth = (redisConf.auth || '').split(':'),
+        redisPass = redisAuth[1];
 
-	redisClient = redis.createClient(redisConf.port, redisConf.hostname, {
-		no_ready_check: true,
-		auth_pass: redisPass,
-	});
-	redisClient.auth(redisPass);
+    redisClient = redis.createClient(redisConf.port, redisConf.hostname, {
+        no_ready_check: true,
+        auth_pass: redisPass,
+    });
+    redisClient.auth(redisPass);
 
 } else if (env === "development") {
 
-	// Redis
-	redisClient = redis.createClient(6379, '127.0.0.1');
+    // Redis
+    redisClient = redis.createClient(6379, '127.0.0.1');
 
 } else {
 
-	// Raise an error
-	console.error("Unknown env '" + env + "'");
+    // Raise an error
+    console.error("Unknown env '" + env + "'");
 }
 
+// Ensure cross domain request support
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 });
 
 // Redis pub/sub
 redisClient.psubscribe("notification *");
 redisClient.on("pmessage", function(pattern, channel, message) {
-	// broadcast the notification
-	if (channel === "notification signatures") {
-		io.emit("signatures", message);
-	} else {
-		console.error("unknown type of notification");
-	}
+    // broadcast the notification
+    if (channel === "notification signatures") {
+        io.emit("signatures", message);
+    } else {
+        console.error("unknown type of notification");
+    }
 });
 
+// Do you copy ?
 app.get("/", function(req, res){
-  res.send("<h1>5 sur 5 Tango Charlie</h1>");
+    res.send("<h1>5 sur 5 Tango Charlie</h1>");
 });
 
+// Socket io
 io.on("connection", function(socket){
-	// silence is golden
+    // silence is golden
 });
 
+// Run the server
 var port = process.env.PORT || 3000;
 http.listen(port, function(){
-  console.log("listening on *:" + port);
+    console.log("listening on *:" + port);
 });
