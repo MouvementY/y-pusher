a191e0a5449058be35942f2ae6f2aee579608758
Pierre Dulac <dulacpier@gmail.com>
2014-11-25 02:38:12 +0100

    Properly configure the redis client based on the env

--- app.js
+++ app.js
@@ -2,7 +2,36 @@ var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
-var redis = require("redis").createClient();
+var redis = require("redis");
+var env = process.env.NODE_ENV || "development";
+var url = require("url");
+
+var redisClient = null;
+if (env === "production") {
+
+	// Redis
+	var redisConf = url.parse(process.env.REDISCLOUD_URL),
+			redisAuth = (redisConf.auth || '').split(':'),
+			redisPass = redisAuth[1];
+
+	console.log(redisConf, redisPass);
+
+	redisClient = redis.createClient(redisConf.port, redisConf.hostname, {
+		no_ready_check: true,
+		auth_pass: redisPass,
+	});
+	redisClient.auth(redisPass);
+
+} else if (env === "development") {
+
+	// Redis
+	redisClient = redis.createClient(6379, '127.0.0.1');
+
+} else {
+
+	// Crash an error
+	console.error("Unknown env '" + env + "'");
+}
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -10,8 +39,8 @@ app.use(function(req, res, next) {
   next();
 });
 
-redis.psubscribe("notification *");
-redis.on("pmessage", function(pattern, channel, message) {
+redisClient.psubscribe("notification *");
+redisClient.on("pmessage", function(pattern, channel, message) {
 	// broadcast the notification
 	if (channel === "notification signatures") {
 		io.emit("signatures", message);
